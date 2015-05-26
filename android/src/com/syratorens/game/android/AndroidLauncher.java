2dc4318e99390e04c334db3dcba602f1aa7649c5
Nicolas Berjamin <n.berjamin@gmail.com>
2015-05-27 01:46:15 +0200

    Premier Commit (Base du projet)

---
+++ android/src/com/syratorens/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.syratorens.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.syratorens.game.Elesmera;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Elesmera(), config);
+	}
+}
---
+++ ios/src/com/syratorens/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.syratorens.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.syratorens.game.Elesmera;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Elesmera(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
